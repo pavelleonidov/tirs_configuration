f1587b5030ae6f3d4e9303dea28aab37135f0fe7
Pavel Leonidov <pavelleonidov@gmail.com>
2017-03-19 01:46:05 +0100

    dev context ViewHelper

---
+++ Classes/ViewHelpers/IsDevelopmentContextViewHelper.php
@@ -0,0 +1,24 @@
+<?php
+namespace TIRS\TirsConfiguration\ViewHelpers;
+
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * Class IsDevelopmentContextViewHelper
+ *
+ * @package In2code\Powermail\ViewHelpers\Condition
+ */
+class IsDevelopmentContextViewHelper extends AbstractViewHelper
+{
+
+    /**
+     * Check if Development context is active
+     *
+     * @return bool
+     */
+    public function render()
+    {
+        return GeneralUtility::getApplicationContext()->isDevelopment();
+    }
+}
